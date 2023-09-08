4071cc12b33f639a51e7b37bbd1b67803013da8c
hoodnika <rhoodnika@gmail.com>
2023-09-08 12:50:27 +0500

    add new fixture(dict)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,19 @@
 import pytest
 
 ARRAY = [0,1,2,3,4,5,6,7,8,9,10]
+DICT = {
+    'saske':'anti-hero',
+    'naruto':'hero',
+    'kakashi':'hero',
+    'daidara':'villian',
+    'zabuza':'villian',
+    'madara':'villian',
+}
 
 @pytest.fixture
 def array_fixture():
     return ARRAY.copy()
+
+@pytest.fixture
+def dict_fixture():
+    return DICT.copy()
